882189663ffc4b6f7bb195a5fc940207c8ad36a7
nadzir <nadz.muhd@gmail.com>
2023-09-18 21:10:54 +0800

    disable hot reload in prod

--- application/main.py
+++ application/main.py
@@ -1,15 +1,16 @@
 from dotenv import load_dotenv
-
+import os
 import uvicorn
 
 
 def main():
     load_dotenv()
+    env = os.environ.get("ENV", False)
     uvicorn.run(
         app="app.server:app",
         host="0.0.0.0",
         port=8000,
-        reload=True,
+        reload=True if env == "dev" else False,
         workers=1,
     )
 
